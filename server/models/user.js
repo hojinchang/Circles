59e66462523d4db295af2f3d768188974fed0bfd
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-03-20 23:56:19 -0700

    Add user and post model schema

---
+++ server/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    firstName: { type: String, required: true, minLength: 1, maxLength: 20 },
+    lastName: { type: String, required: true, minLength: 1, maxLength: 20 },
+    email: { type: String, required: true, minLength: 1 },
+    password: { type: String, required: true, minLength: 1 }
+});
+
+UserSchema.virtual("fullName").get(function() {
+    let fullName = "";
+    if (this.firstName && this.lastName) {
+        fullName = `${this.firstName} ${this.lastName}`;
+    }
+
+    return fullName;
+});
+
+module.exports = mongoose.model("User", UserSchema);
