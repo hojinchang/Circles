b7f5a19c60453e084461e2723fc2453b3adb8acc
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-03-21 23:43:39 -0700

    Add sign up page

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 import googleLogo from "../assets/images/Google__G__logo.svg";
 
-const LoginPage = () => {
+const SignUpPage = () => {
 
     return(
         <main className="flex flex-col justify-center items-center px-8 py-10 lg:py-24">
@@ -14,13 +14,37 @@ const LoginPage = () => {
             </header>
             
             <section className="mb-6 max-w-96">
-                <h1 className="text-3xl font-semibold text-center mb-2">Login</h1>
+                <h1 className="text-3xl font-semibold text-center mb-2">Sign Up</h1>
                 <p className="text-neutral-500  text-center text-sm">Enter your credentials below to sign in.</p>
             </section>
 
             <section className="max-w-96 w-full">
                 <form className="w-full mx-auto">
                     <div className="flex flex-col gap-2">
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <label htmlFor="firstName" className="text-sm font-medium text-right">First Name</label>
+                            <input 
+                                type="text" 
+                                id="firstName" 
+                                name="firstName" 
+                                minLength="1"
+                                placeholder="John"
+                                required
+                                className="input col-span-3"
+                            />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <label htmlFor="lastName" className="text-sm font-medium text-right">Last Name</label>
+                            <input 
+                                type="text" 
+                                id="lastName" 
+                                name="lastName" 
+                                minLength="1"
+                                placeholder="Doe"
+                                required
+                                className="input col-span-3"
+                            />
+                        </div>
                         <div className="grid grid-cols-4 items-center gap-4">
                             <label htmlFor="email" className="text-sm font-medium text-right">Email</label>
                             <input 
@@ -45,8 +69,20 @@ const LoginPage = () => {
                                 className="input col-span-3"
                             />
                         </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <label htmlFor="passwordConfirm" className="text-sm font-medium text-right">Password Confirm</label>
+                            <input 
+                                type="password" 
+                                id="passwordConfirm" 
+                                name="passwordConfirm" 
+                                minLength="1"
+                                placeholder="********"
+                                required
+                                className="input col-span-3"
+                            />
+                        </div>
                         <div className="mt-4">
-                            <button type="submit" className="bg-slate-500 w-full px-6 py-2 text-neutral-50 font-medium rounded-md hover:bg-slate-700 transition ease duration-200">Login</button>
+                            <button type="submit" className="bg-slate-500 w-full px-6 py-2 text-neutral-50 font-medium rounded-md hover:bg-slate-700 transition ease duration-200">Sign Up</button>
                         </div>
                     </div>
                 </form>
@@ -68,14 +104,15 @@ const LoginPage = () => {
                             <div className="bg-white rounded-full p-0.5">
                                 <img src={googleLogo} alt="Google logo" />
                             </div>
-                            Sign in with Google
+                            Sign up with Google
                         </button>
                     </div>
                 </form>
             </section>
-            <p className="font-medium text-sm text-neutral-500">Don't have an account? <Link to="/sign-up" className="text-slate-800 hover:text-slate-500 hover:underline transition ease duration-200">Sign up.</Link></p>
+            <p className="font-medium text-sm text-neutral-500">Already have an acount? <Link to="/login" className="text-slate-800 hover:text-slate-500 hover:underline transition ease duration-200">Login.</Link></p>
+
         </main>
     )
 };
 
-export default LoginPage;
+export default SignUpPage;
--- client/src/routers/AppRouter.jsx
+++ client/src/routers/AppRouter.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
+import SignUpPage from "../pages/SignUpPage";
 
 const AppRouter = () => {
     return (
@@ -9,6 +10,7 @@ const AppRouter = () => {
             <Routes>
                 <Route path="/" exact element={<HomePage />}></Route>
                 <Route path="/login" exact element={<LoginPage />}></Route>
+                <Route path="/sign-up" exact element={<SignUpPage />}></Route>
             </Routes>
         </BrowserRouter>
     )
